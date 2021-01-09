d0553b1774e72e16d1da3f92b7e4199a8520bf3c
Sebastiano Gallo <sebastiano.gallo@offertetouroperator.com>
2021-01-09 14:03:33 +0100

    fix: made comparison logic closer to vanilla and added liquid coolers

--- patches/ReplacePartObjective_TestSatisfiedBy_Patch.cs
+++ patches/ReplacePartObjective_TestSatisfiedBy_Patch.cs
@@ -8,14 +8,20 @@ namespace SebaFixes.patches
     {
 
         [HarmonyPostfix]
-        private static bool Postfix(bool __result, ComputerSave save, PartDesc.Type ___m_partType)
+        private static bool Postfix(bool __result, ComputerSave save, PartDesc.Type ___m_partType, string ___m_refPart)
         {
             // Debug.Log("[SF] TestSatisifiedBy");
-            if (___m_partType == PartDesc.Type.MOTHERBOARD || ___m_partType == PartDesc.Type.AIR_COOLER)
+            if (___m_partType == PartDesc.Type.MOTHERBOARD ||
+                ___m_partType == PartDesc.Type.AIR_COOLER ||
+                ___m_partType == PartDesc.Type.LIQUID_COOLER)
             {
                 foreach (var partInstance in save.GetAllParts())
                 {
-                    return partInstance.IsActive() && partInstance.GetPart().MatchForUpgrade(___m_partType);
+                    if (partInstance!=null && partInstance.IsActive())
+                    {
+                        PartDesc refPart = PartsDatabase.GetDesc(___m_refPart);
+                        return partInstance.GetPart().MatchForUpgrade(refPart.m_type);
+                    }
                 }
             }
             return __result;
