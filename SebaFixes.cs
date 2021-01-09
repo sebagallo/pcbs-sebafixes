9a46c92c201091bf9642ae5cc588e959865a54b9
Sebastiano Gallo <sebastiano.gallo@offertetouroperator.com>
2021-01-09 12:23:25 +0100

    First Commit

---
+++ SebaFixes.cs
@@ -0,0 +1,52 @@
+﻿using System.Linq;
+using System.Reflection;
+using BepInEx;
+using HarmonyLib;
+using UnityEngine;
+
+namespace SebaFixes
+{
+    [BepInPlugin(PluginGuid, PluginName, PluginVersion)]
+    internal class SebaFixes : BaseUnityPlugin
+    {
+        public const string PluginGuid = "com.sebag.pcbs.fixes";
+        public const string PluginName = "Seba Fixes";
+        public const string PluginVersion = "1.0.0";
+
+        internal void Awake()
+        {
+            Debug.Log("Seba Fixes loaded!");
+        }
+
+        public SebaFixes()
+        {
+            // Harmony.CreateAndPatchAll(typeof(Patches), PluginGuid);
+            // var harmony = new Harmony(PluginGuid);
+            // harmony.PatchAll();
+            // harmony.PatchAll(typeof(ReplacePartObjectiveTestSatisfiedByPatch));
+            // harmony.PatchAll(typeof(Patches));
+
+            // var originalMethod = typeof(ReplacePartObjective).GetMethod("TestSatisfiedBy");
+            // var prefixMethod = typeof(ReplacePartObjectiveTestSatisfiedByPatch).GetMethod("Prefix");
+            // var postfixMethod = typeof(ReplacePartObjectiveTestSatisfiedByPatch).GetMethod("Postfix");
+            //
+            // Debug.Log(originalMethod);
+            // Debug.Log(prefixMethod);
+            // Debug.Log(postfixMethod);
+            //
+            // harmony.Patch(
+            //     originalMethod,
+            //     new HarmonyMethod(prefixMethod),
+            //     new HarmonyMethod(postfixMethod)
+            // );
+            Debug.Log("[SF] Patching Methods...");
+            var harmony = new Harmony(PluginGuid);
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+            Debug.Log("[SF] Patched methods: " + harmony.GetPatchedMethods().Count().ToString());
+            foreach (var method in harmony.GetPatchedMethods())
+            {
+                Debug.Log("[SF] Patched: " + method.Name);
+            }
+        }
+    }
+}
---
+++ patches/ReplacePartObjective_TestSatisfiedBy_Patch.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using HarmonyLib;
+using UnityEngine;
+
+namespace SebaFixes.patches
+{
+    [HarmonyPatch(typeof(ReplacePartObjective), "TestSatisifiedBy")]
+    public class ReplacePartObjectiveTestSatisfiedByPatch
+    {
+
+        [HarmonyPrefix]
+        private static bool Prefix()
+        {
+            return false;
+        }
+
+        [HarmonyPostfix]
+        private static bool Postfix(bool __result, ReplacePartObjective __instance, Job job, ComputerSave save, string ___m_refPart, PartDesc.Type ___m_partType, int ___m_refGB, int ___m_minRamRating, int ___m_totalPartsOfType)
+        {
+            Debug.Log("[SF] Test PART");
+            if (___m_refPart == null)
+                return true;
+            if (___m_partType == PartDesc.Type.RAM)
+                return save.GetRAM() >= ___m_refGB && save.GetMinRAMRating() >= ___m_minRamRating;
+            if (((IEnumerable<PartDesc.Type>) PartDesc.s_storageTypes).Contains<PartDesc.Type>(___m_partType))
+                return save.GetStorage() >= ___m_refGB;
+            if (___m_partType == PartDesc.Type.MOTHERBOARD)
+                return true;
+            if (___m_partType == PartDesc.Type.AIR_COOLER)
+                return true;
+            PartDesc refPart = PartsDatabase.GetDesc(___m_refPart, (GetString) null);
+            return save.GetAllParts().Count<PartInstance>((Func<PartInstance, bool>) (x => x != null && x.IsActive() && x.GetPart().MatchForUpgrade(refPart.m_type) && (double) x.GetPart().GetQuality() >= (double) refPart.GetQuality())) == ___m_totalPartsOfType;
+        }
+        
+    }
+}
