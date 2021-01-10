c9a172af4d599a6cd154aedcd1e757e3870acd89
Sebastiano Gallo <sebastiano.gallo@offertetouroperator.com>
2021-01-10 17:50:58 +0100

    feat: more PCBAY offers

---
+++ patches/CareerStatus_GenerateTodaysOffers_Patch.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+using SebaFixes.utils;
+
+namespace SebaFixes.patches
+{
+    [HarmonyPatch(typeof(CareerStatus), "GenerateTodaysOffers")]
+    public class CareerStatusGenerateTodaysOffersPatch
+    {
+        [HarmonyPrefix]
+        public static void Prefix()
+        {
+            if (CareerConstants.s_numberOffers < 7)
+            {
+                CareerConstants.s_numberOffers = 7;
+            }
+        }
+        
+    }
+}
